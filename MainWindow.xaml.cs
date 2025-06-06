5f99451691713ac59d5919ab97680834bc83b5f2
andreibaidoc <60269675+andreibaidoc@users.noreply.github.com>
2025-06-06 12:53:51 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Ports;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ArduinoGUI_Interface
+{
+    public partial class MainWindow : Window
+    {
+        // variables
+        private SerialPort? serialPort;
+        private string? arduinoIP;
+        private readonly int arduinoPort = 8888;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshComPorts();
+        }
+
+        private void RefreshComPorts()
+        {
+            serial_options_combobox.ItemsSource = SerialPort.GetPortNames();
+        }
+
+    }
+}
